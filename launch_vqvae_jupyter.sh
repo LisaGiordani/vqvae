bb1a4f957bdf12ee51705a520391a03c368665c2
lisa.giordani <lisa.giordani@etu.emse.fr>
2022-03-11 10:53:03 +0100

    remove log folder

--- launch_vqvae_jupyter.sh
+++ launch_vqvae_jupyter.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 # SBATCH -p iaensta
-# SBATCH -t 00:10:00 # time limit max 48:00:00
+# SBATCH -t 48:10:00 # time limit max 48:00:00
 # SBATCH -c 4 # number of cores
 # SBATCH --gres=gpu:1 # number of gpus required max 4 (will be limited to 1)
 # SBATCH -o ~/vqvae/results.out #where to write sys.out/print
